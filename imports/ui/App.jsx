75aede77408913c4d04c2e5511577cdc9272258b
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-01-10 21:42:27 +0100

    Replace HTML code in client/main.html, create new client/main.jsx file as well as two components - imports/ui/App.jsx and imports/ui/Task.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Task from '.Task.jsx';
+import Task from './Task.jsx';
 
 // App component - represents the whole App
 export default class App extends Component {
@@ -8,7 +8,7 @@ export default class App extends Component {
         return [
             { _id: 1, text: 'This is task 1'},
             { _id: 2, text: 'This is task 2'},
-            { _id: 3, text: 'This is task 3'},
+            { _id: 3, text: 'This is task 3'}
         ];
     }
 
@@ -20,13 +20,15 @@ export default class App extends Component {
 
     render() {
         return (
-            <header>
-                <h1>Todo List</h1>
-            </header>
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
 
-            <ul>
-                {this.renderTasks()}
-            </ul>
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
         );
     }
 }
